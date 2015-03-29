0e72b63d25563e5c81b2843374d0ed1d0856bc4b
softjamball <jobrien1460@gmail.com>
2015-03-29 17:39:54 -0500

    Homework1

---
+++ Homework1/src/homework1/Homework1.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package homework1;
+
+
+public class Homework1 {
+
+    
+    public static void main(String[] args) {
+    
+        Double z;
+        Double x = 10.0;
+        Double y = 20.0;
+        
+        System.out.println("x contains " + x);
+        System.out.println("y contains " + y);
+        z = x + y;
+        System.out.println("The sum of x and y is: " + z);
+        z = x - y;
+        System.out.println("The difference of x and y is: " + z);
+        z = x * y;
+        System.out.println("The product of x and y is: " + z);
+        z = x / y;
+        System.out.println("The quotent of x and y is: " + z);
+        z = x % y;
+        System.out.println("The modulus of x and y is: " + z);
+        
+        
+    }
+    
+    
+    
+    
+    
+    
+}
